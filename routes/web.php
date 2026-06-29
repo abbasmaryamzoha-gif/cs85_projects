6b57174958fff309fd967a3ac88d0024256d0a4e
maryam abbas <abbas_maryam_zoha@student.smc.edu>
2026-06-28 21:02:06 -0700

    ...

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return  '<h1> Welcome to My Website </h1></br>Maryam Abbas Laravel app';
 });
